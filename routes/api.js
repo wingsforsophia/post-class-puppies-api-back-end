bf639ea1cbdcd0e93b36a578e195bbdc9f5c6db3
Sophia Seremetis <shseremetis@gmail.com>
2021-02-22 14:03:46 -0500

    routes written

--- routes/api.js
+++ routes/api.js
@@ -1,9 +1,11 @@
 var express = require('express');
 var router = express.Router();
+const puppyCtrl = require('../controllers/puppies')
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-});
+router.get('/puppies', puppyCtrl.index)
+router.get('/puppies/:id', puppyCtrl.show)
+router.post('/puppies', puppyCtrl.create)
+router.put('/puppies/:id', puppyCtrl.update)
+router.delete('/puppies/:id', puppyCtrl.delete)
 
 module.exports = router;
